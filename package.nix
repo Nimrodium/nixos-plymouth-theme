14c7e9059d946694d2a9014344972e23ac87f1f8
faizm55212 <faizm55212@gmail.com>
2025-08-31 05:27:14 +0530

    nix update image path during build

--- package.nix
+++ package.nix
@@ -8,5 +8,6 @@ stdenvNoCC.mkDerivation {
     mkdir -p $out/share/plymouth/themes/mac-style
     cp -r mac-style $out/share/plymouth/themes/
     chmod +x $out/share/plymouth/themes/mac-style/mac-style.plymouth
+    substituteInPlace $out/share/plymouth/themes/mac-style/mac-style.plymouth --replace '@IMAGES@' "$out/share/plymouth/themes/mac-style/images/"
   '';
 }
